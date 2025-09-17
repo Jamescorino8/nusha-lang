0943ae3c9e8460e28d05387a1926cea8fd396970
James Corino <122735477+Jamescorino8@users.noreply.github.com>
2025-09-17 18:57:00 -0400

    Complete Lexer 1

--- src/main/java/Lexer.java
+++ src/main/java/Lexer.java
@@ -1,13 +1,51 @@
 import AST.*;
+import AST.Token;
+import AST.Token.TokenTypes;
+
+import java.util.HashMap;
 import java.util.LinkedList;
 
 public class Lexer {
+    private final TextManager textManager;
+    private int lineNumber = 0;
+    private int columnNumber = 0;
+    private final HashMap<String, TokenTypes> keywords;
 
     public Lexer(String input) {
+        this.textManager = new TextManager(input);
+        this.keywords = new HashMap<>();
         
+        // insert all the used keywords into keywords map
+        keywords.put("unique", TokenTypes.UNIQUE);
+        keywords.put("var", TokenTypes.VAR);
     }
 
     public LinkedList<Token> Lex() throws SyntaxErrorException {
+        LinkedList<Token> tokens = new LinkedList<>();
+        while (!textManager.isAtEnd()) {
+            char currentChar = textManager.PeekCharacter();
+            if (Character.isLetter(currentChar)) {
+                tokens.add(readWord());
+            } else if (currentChar == '\n') {
+                tokens.add(new Token(Token.TokenTypes.NEWLINE, lineNumber, columnNumber));
+                textManager.GetCharacter();
+            } else if (Character.isWhitespace(currentChar)) { 
+                textManager.GetCharacter(); // skip whitespace chars
+            }
+        }
+        return tokens;
+    }
 
+    private Token readWord() {
+        StringBuilder stringBuilder = new StringBuilder(); // StringBuilder bc cant add char to String
+        while (!textManager.isAtEnd() && Character.isLetterOrDigit(textManager.PeekCharacter())) {
+            stringBuilder.append(textManager.GetCharacter());
+        }
+        String word = stringBuilder.toString();
+        if (keywords.containsKey(word)) {
+            return new Token(keywords.get(word), lineNumber, columnNumber);
+        } else {
+            return new Token(Token.TokenTypes.IDENTIFIER, lineNumber, columnNumber, word);
+        }
     }
 }
--- src/main/java/TextManager.java
+++ src/main/java/TextManager.java
@@ -2,18 +2,20 @@ public class TextManager {
     private final String text;
     private int position;
     public TextManager(String input) {
-        text = input;
-        position = 0;
+        this.text = input;
+        this.position = 0;
     }
     public boolean isAtEnd() {
-        return position == text.length() - 1;
+        return position >= text.length();
     }
     public char PeekCharacter() {
         return text.charAt(position);
     }
+
     public char PeekCharacter(int dist) {
         return text.charAt(dist);
     }
+    
     public char GetCharacter() {
         return text.charAt(position++);
     }
