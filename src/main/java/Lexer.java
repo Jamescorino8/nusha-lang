f4f4baff0e90742f63139679f6f5f9b2829fc5a7
James Corino <122735477+Jamescorino8@users.noreply.github.com>
2025-09-24 17:03:07 -0400

    Lexer 2: Handle indentation, numbers, punctuation, and whitespace correctly

--- src/main/java/Lexer.java
+++ src/main/java/Lexer.java
@@ -1,5 +1,4 @@
 import AST.*;
-import AST.Token;
 import AST.Token.TokenTypes;
 
 import java.util.HashMap;
@@ -7,15 +6,17 @@ import java.util.LinkedList;
 
 public class Lexer {
     private final TextManager textManager;
-    private int lineNumber = 0;
-    private int columnNumber = 0;
+    private int lineNumber = 1;
+    private int columnNumber = 1;
     private final HashMap<String, TokenTypes> keywords;
+    private final LinkedList<Integer> indentLevels;
 
     public Lexer(String input) {
         this.textManager = new TextManager(input);
         this.keywords = new HashMap<>();
-        
-        // insert all the used keywords into keywords map
+        this.indentLevels = new LinkedList<>();
+        indentLevels.add(0);
+
         keywords.put("unique", TokenTypes.UNIQUE);
         keywords.put("var", TokenTypes.VAR);
     }
@@ -23,29 +24,137 @@ public class Lexer {
     public LinkedList<Token> Lex() throws SyntaxErrorException {
         LinkedList<Token> tokens = new LinkedList<>();
         while (!textManager.isAtEnd()) {
+            // if indentation level > 0 at beginning, we need to handle DEDENTs first.
+            if (columnNumber == 1 && !textManager.isAtEnd() && !Character.isWhitespace(textManager.PeekCharacter())) {
+                while (indentLevels.getLast() > 0) {
+                    indentLevels.removeLast();
+                    tokens.add(new Token(Token.TokenTypes.DEDENT, lineNumber, columnNumber));
+                }
+            }
+            // Handle indentation only at the beginning of a line.
+            if (columnNumber == 1 && Character.isWhitespace(textManager.PeekCharacter())) {
+                int indent = 0;
+                while (!textManager.isAtEnd() && Character.isWhitespace(textManager.PeekCharacter())) {
+                    char currentChar = textManager.GetCharacter();
+                    if (currentChar == '\t') {
+                        indent += 4;
+                    } else if (currentChar == ' ') {
+                        indent++;
+                    } else if (currentChar == '\n') {
+                        tokens.add(new Token(Token.TokenTypes.NEWLINE, lineNumber, columnNumber));
+                        lineNumber++;
+                        columnNumber = 1;
+                        indent = 0; // Reset for the new line
+                    }
+                }
+
+                if (indent % 4 != 0) {
+                    throw new SyntaxErrorException("Indentation Error", lineNumber, columnNumber);
+                }
+
+                if (indent > indentLevels.getLast()) {
+                    indentLevels.add(indent);
+                    tokens.add(new Token(Token.TokenTypes.INDENT, lineNumber, columnNumber));
+                } else {
+                    while (indent < indentLevels.getLast()) {
+                        indentLevels.removeLast();
+                        tokens.add(new Token(Token.TokenTypes.DEDENT, lineNumber, columnNumber));
+                    }
+                }
+                 columnNumber += indent;
+            }
+
+            if (textManager.isAtEnd()) break;
+
             char currentChar = textManager.PeekCharacter();
             if (Character.isLetter(currentChar)) {
                 tokens.add(readWord());
+            } else if (Character.isDigit(currentChar)) {
+                tokens.add(readNumber());
             } else if (currentChar == '\n') {
-                tokens.add(new Token(Token.TokenTypes.NEWLINE, lineNumber, columnNumber));
                 textManager.GetCharacter();
-            } else if (Character.isWhitespace(currentChar)) { 
-                textManager.GetCharacter(); // skip whitespace chars
+                tokens.add(new Token(Token.TokenTypes.NEWLINE, lineNumber, columnNumber));
+                lineNumber++;
+                columnNumber = 1;
+            } else if (Character.isWhitespace(currentChar)){
+                textManager.GetCharacter(); // Consume other whitespace
+                columnNumber++;
+            }
+            else {
+                tokens.add(readPunctuation());
             }
         }
+
+        // Handle DEDENT for any remaining indentation at the end of the file.
+        while (indentLevels.getLast() > 0) {
+            indentLevels.removeLast();
+            tokens.add(new Token(Token.TokenTypes.DEDENT, lineNumber, columnNumber));
+        }
+
+        tokens.add(new Token(Token.TokenTypes.NEWLINE, lineNumber, columnNumber));
         return tokens;
     }
 
     private Token readWord() {
-        StringBuilder stringBuilder = new StringBuilder(); // StringBuilder bc cant add char to String
+        StringBuilder stringBuilder = new StringBuilder();
+        int startColumn = columnNumber;
         while (!textManager.isAtEnd() && Character.isLetterOrDigit(textManager.PeekCharacter())) {
             stringBuilder.append(textManager.GetCharacter());
+            columnNumber++;
         }
         String word = stringBuilder.toString();
         if (keywords.containsKey(word)) {
-            return new Token(keywords.get(word), lineNumber, columnNumber);
+            return new Token(keywords.get(word), lineNumber, startColumn);
         } else {
-            return new Token(Token.TokenTypes.IDENTIFIER, lineNumber, columnNumber, word);
+            return new Token(Token.TokenTypes.IDENTIFIER, lineNumber, startColumn, word);
+        }
+    }
+
+    private Token readNumber() {
+        StringBuilder stringBuilder = new StringBuilder();
+        int startColumn = columnNumber;
+        while (!textManager.isAtEnd() && Character.isDigit(textManager.PeekCharacter())) {
+            stringBuilder.append(textManager.GetCharacter());
+            columnNumber++;
+        }
+        return new Token(Token.TokenTypes.NUMBER, lineNumber, startColumn, stringBuilder.toString());
+    }
+
+    private Token readPunctuation() throws SyntaxErrorException {
+        char currentChar = textManager.GetCharacter();
+        int startColumn = columnNumber;
+        columnNumber++;
+        switch (currentChar) {
+            case '{':
+                return new Token(Token.TokenTypes.LEFTCURLY, lineNumber, startColumn);
+            case '}':
+                return new Token(Token.TokenTypes.RIGHTCURLY, lineNumber, startColumn);
+            case '[':
+                return new Token(Token.TokenTypes.LEFTBRACE, lineNumber, startColumn);
+            case ']':
+                return new Token(Token.TokenTypes.RIGHTBRACE, lineNumber, startColumn);
+            case ',':
+                return new Token(Token.TokenTypes.COMMA, lineNumber, startColumn);
+            case '.':
+                return new Token(Token.TokenTypes.DOT, lineNumber, startColumn);
+            case ':':
+                return new Token(Token.TokenTypes.COLON, lineNumber, startColumn);
+            case '=':
+                if (!textManager.isAtEnd() && textManager.PeekCharacter() == '>') {
+                    textManager.GetCharacter();
+                    columnNumber++;
+                    return new Token(Token.TokenTypes.YIELDS, lineNumber, startColumn);
+                }
+                return new Token(Token.TokenTypes.EQUAL, lineNumber, startColumn);
+            case '!':
+                if (!textManager.isAtEnd() && textManager.PeekCharacter() == '=') {
+                    textManager.GetCharacter();
+                    columnNumber++;
+                    return new Token(Token.TokenTypes.NOTEQUAL, lineNumber, startColumn);
+                }
+                throw new SyntaxErrorException("Error: '!' without '='", lineNumber, startColumn);
+            default:
+                throw new SyntaxErrorException("Unknown character '" + currentChar + "'", lineNumber, startColumn);
         }
     }
 }
