b06368b1d143d0cfce904b484851e1e1bb32d5c7
James Corino <122735477+Jamescorino8@users.noreply.github.com>
2025-09-27 19:01:37 -0400

    implement TokenManager for Parser

--- src/main/java/TokenManager.java
+++ src/main/java/TokenManager.java
@@ -1,30 +1,38 @@
 import AST.Token;
+
+import java.lang.classfile.ClassFile.Option;
 import java.util.LinkedList;
 import java.util.Optional;
 
 public class TokenManager {
+    LinkedList<Token> tokens;
 
     public TokenManager(LinkedList<Token> tokens) {
-
+        this.tokens = tokens;
     }
 
     public int getLine() {
-
+        return tokens.getFirst().LineNumber;
     }
 
     public int getColumn() {
-
+        return tokens.getFirst().ColumnNumber;
     }
 
     public boolean Done() {
-
+        return tokens.isEmpty();
     }
 
     public Optional<Token> MatchAndRemove(Token.TokenTypes t) {
-
+        Optional<Token> match = Optional.empty();
+        if (tokens.getFirst().Type == t) {
+            match = Optional.of(tokens.getFirst());
+            tokens.removeFirst();
+        }
+        return match;
     }
 
     public Optional<Token> Peek (int i) {
-
+        return Optional.of(tokens.get(i));
     }
 }
