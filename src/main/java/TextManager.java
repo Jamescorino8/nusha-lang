db8b5426cd5c457e8d95e9709be73d6e77fafb11
James Corino <122735477+Jamescorino8@users.noreply.github.com>
2025-09-27 12:36:54 -0400

    refactor TextManager to handle positioning

--- src/main/java/TextManager.java
+++ src/main/java/TextManager.java
@@ -1,13 +1,18 @@
 public class TextManager {
     private final String text;
     private int position;
+    private int lineNumber;
+
     public TextManager(String input) {
         this.text = input;
         this.position = 0;
+        this.lineNumber = 1;
     }
+
     public boolean isAtEnd() {
         return position >= text.length();
     }
+
     public char PeekCharacter() {
         return text.charAt(position);
     }
@@ -15,8 +20,20 @@ public class TextManager {
     public char PeekCharacter(int dist) {
         return text.charAt(dist);
     }
-    
+
     public char GetCharacter() {
-        return text.charAt(position++);
+        char c = text.charAt(position++);
+        if (c == '\n') {
+            lineNumber++;
+        }
+        return c;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public int getLineNumber() {
+        return lineNumber;
     }
 }
